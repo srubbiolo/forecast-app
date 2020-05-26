930e0ce9f6f59176dd58643a4700b5f6ca6bc3f7
Santiago Rubbiolo <Santiago.rubbiolo@sprint.com>
2020-05-25 23:48:34 -0300

    Updates

---
+++ precache-manifest.96229c9171f57b1c5f9aba856133cae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f2eb9c75e335b2c8c925d8299b4d74",
+    "url": "/forecast-app/index.html"
+  },
+  {
+    "revision": "4d8fbedb0021de72384c",
+    "url": "/forecast-app/static/css/2.ae98c48f.chunk.css"
+  },
+  {
+    "revision": "2ea0e3b507c9a8c4a77e",
+    "url": "/forecast-app/static/css/main.3d733cfd.chunk.css"
+  },
+  {
+    "revision": "4d8fbedb0021de72384c",
+    "url": "/forecast-app/static/js/2.25bdabfd.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/forecast-app/static/js/2.25bdabfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea0e3b507c9a8c4a77e",
+    "url": "/forecast-app/static/js/main.c021506e.chunk.js"
+  },
+  {
+    "revision": "91f7e0dd072363e87f48",
+    "url": "/forecast-app/static/js/runtime-main.febd37f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-forecast-app/precache-manifest.27e516cd46768d31afb0223c2effa31b.js"
+  "/forecast-app/precache-manifest.96229c9171f57b1c5f9aba856133cae0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-forecast-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/forecast-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
