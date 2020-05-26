7ec086294762f7277f8c37bb131964a44d11d6fb
Santiago Rubbiolo <Santiago.rubbiolo@sprint.com>
2020-05-25 23:38:06 -0300

    Updates

---
+++ precache-manifest.27e516cd46768d31afb0223c2effa31b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d1fc0a0d7c6c0dfbc7637fc6078430",
+    "url": "/react-forecast-app/index.html"
+  },
+  {
+    "revision": "4d8fbedb0021de72384c",
+    "url": "/react-forecast-app/static/css/2.ae98c48f.chunk.css"
+  },
+  {
+    "revision": "f99a3dec4ea84524a39b",
+    "url": "/react-forecast-app/static/css/main.3d733cfd.chunk.css"
+  },
+  {
+    "revision": "4d8fbedb0021de72384c",
+    "url": "/react-forecast-app/static/js/2.25bdabfd.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-forecast-app/static/js/2.25bdabfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99a3dec4ea84524a39b",
+    "url": "/react-forecast-app/static/js/main.7c725754.chunk.js"
+  },
+  {
+    "revision": "38450652793f3cf194c7",
+    "url": "/react-forecast-app/static/js/runtime-main.c1072c39.js"
+  }
+]);
